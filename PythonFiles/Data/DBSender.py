1a9bb46c85db48e607994cf278088e93871171dc
Rand Bovard <bovar008@umn.edu>
2025-03-25 16:47:17 -0500

    Automatically load teststand info on GUI start

--- PythonFiles/Data/DBSender.py
+++ PythonFiles/Data/DBSender.py
@@ -231,6 +231,21 @@ class DBSender():
 
         for i in range(begin, end): 
             return lines[i]
+
+    def get_tester_config(self, teststand):
+        r = requests.post('{}/get_tester_config_id.py'.format(self.db_url), data={'test_stand':teststand})
+
+        lines = r.text.split('\n')
+
+        try:
+            begin = lines.index("Begin") + 1
+            end = lines.index("End")
+
+            for i in range(begin, end): 
+                return lines[i]
+
+        except:
+            return None
         
 
     def add_test_json(self, json_file):
